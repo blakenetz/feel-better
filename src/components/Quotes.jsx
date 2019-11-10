43b66ffdfb377ccbb5c14854b129d38eb93a744d
blake <blake.netzeband@spiredigital.com>
2019-11-09 20:11:43 -0700

    cleanup in quotes

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import classnames from "classnames";
+import PropType from "prop-types";
 
 const quotes = [
   { quote: "No Feeling is Final" },
@@ -22,21 +24,28 @@ const quotes = [
   { quote: "Whatever you're fishing for, catch and release" },
 ];
 
-function Quote({ quote, author, current, index }) {
-  const className =
-    current === index ? "current" : current - 1 === index ? "previous" : "next";
+function Quote({ quote, author, current, index, handleClick }) {
+  const classes = {
+    current: current === index,
+    previous: current - 1 === index,
+    next: current + 1 === index,
+  };
 
   return (
-    <li className={className}>
+    <li className={classnames(classes)} onClick={handleClick}>
       <h1>{quote}.</h1>
       {author && <h2>- {author}</h2>}
     </li>
   );
 }
 
-function PaginationButton({ type, title, handleClick }) {
+function PaginationButton({ type, handleClick }) {
   return (
-    <button onClick={handleClick} className={`btn btn--${type}`} title={title}>
+    <button
+      onClick={handleClick}
+      className={classnames("btn", type)}
+      title={`Show ${type} quote`}
+    >
       <svg className="icon" viewBox="0 0 24 24">
         <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
       </svg>
@@ -45,16 +54,21 @@ function PaginationButton({ type, title, handleClick }) {
 }
 
 function Quotes(props) {
-  const [current, setSlide] = React.useState(0);
+  const [current, setCurrent] = React.useState(0);
 
   function goBack() {
     const previous = current - 1;
-    setSlide(previous < 0 ? quotes.length - 1 : previous);
+    setCurrent(previous < 0 ? quotes.length - 1 : previous);
   }
 
   function goForward() {
     const next = current + 1;
-    setSlide(next >= quotes.length ? 0 : next);
+    setCurrent(next >= quotes.length ? 0 : next);
+  }
+
+  function navigateQuote(i) {
+    if (current === i) return;
+    setCurrent(i);
   }
 
   const style = {
@@ -66,26 +80,35 @@ function Quotes(props) {
       <div className="wrapper">
         <ul style={style}>
           {quotes.map((quote, i) => (
-            <Quote {...quote} key={i} current={current} index={i} />
+            <Quote
+              {...quote}
+              key={i}
+              current={current}
+              index={i}
+              handleClick={() => navigateQuote(i)}
+            />
           ))}
         </ul>
 
         <div className="controls">
-          <PaginationButton
-            type="previous"
-            title="show previous quote"
-            handleClick={goBack}
-          />
-
-          <PaginationButton
-            type="next"
-            title="Show next quote"
-            handleClick={goForward}
-          />
+          <PaginationButton type="previous" handleClick={goBack} />
+          <PaginationButton type="next" handleClick={goForward} />
         </div>
       </div>
     </section>
   );
 }
+
+Quote.propTypes = {
+  quote: PropType.string.isRequired,
+  author: PropType.string,
+  current: PropType.number.isRequired,
+  index: PropType.number.isRequired,
+  handleClick: PropType.func.isRequired,
+};
+PaginationButton.propTypes = {
+  type: PropType.oneOf(["previous", "next"]).isRequired,
+  handleClick: PropType.func.isRequired,
+};
 
 export default Quotes;
